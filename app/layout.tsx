8dc6076d3a2ccc8e3f89c371d21298169d6d86c5
Robin Ustohal <ustik96@gmail.com>
2024-10-03 21:34:50 +0200

    feat(layout): Added header from NextUI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import {Providers} from "./providers";
+import Link from "next/link";
+import { Providers } from "./providers";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Button } from "@nextui-org/react";
+import { AcmeLogo } from "./AcmeLogo";
 
 import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
@@ -10,11 +13,32 @@ export const metadata: Metadata = {
     description: "Portál důchodce",
 };
 
-export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>)
-{
+export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
     return (
         <html lang="en" className="light">
             <body className={inter.className}>
+                <Navbar>
+                    <NavbarBrand>
+                        <p className="font-bold text-inherit">Portál důchodce</p>
+                    </NavbarBrand>
+                    <NavbarContent className="hidden sm:flex gap-4" justify="center">
+                        <NavbarItem isActive>
+                            <Link color="foreground" href="#">
+                                Domov
+                            </Link>
+                        </NavbarItem>
+                        <NavbarItem>
+                            <Link href="#" aria-current="page">
+                                Aktivity
+                            </Link>
+                        </NavbarItem>
+                        <NavbarItem>
+                            <Link color="foreground" href="#">
+                                Novinky
+                            </Link>
+                        </NavbarItem>
+                    </NavbarContent>
+                </Navbar>
                 <Providers>
                     {children}
                 </Providers>
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home()
 {
     return (
         <div>
-            Portál důchodce
+            Ahoj světe
         </div>
     );
 }
