38b04433c96451678ef80e9bfc8c9eaa5243719d
Robin Ustohal <ustik96@gmail.com>
2024-10-06 16:47:31 +0200

    feat(home): Add activites cards

---
+++ app/components/ActivityCard.tsx
@@ -0,0 +1,21 @@
+import { Card } from "@nextui-org/react";
+
+interface ActivityCardProps {
+  imgSrc: string;
+  title: string;
+}
+
+export default function ActivityCard({ imgSrc, title }: ActivityCardProps) {
+  return (
+    <Card className="relative w-[14rem] h-[20rem] cursor-pointer hover:scale-[1.03] transition duration-200">
+      <img
+        src={imgSrc}
+        alt={title}
+        className="absolute inset-0 w-full h-full object-cover"
+      />
+      <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-30">
+        <h2 className="font-semibold text-white text-lg text-center">{title}</h2>
+      </div>
+    </Card>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,26 @@
 import { Card, CardBody, CardFooter } from "@nextui-org/react";
 import NewsCard from "./components/NewsCard";
+import ActivityCard from "./components/ActivityCard";
 
 export default function Home() {
     return (
-        <div className="flex gap-6 p-10">
-            <NewsCard title="Lorem ipsum" description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec ipsum massa, ullamcorper in." imgSrc="https://www.akamai.com/site/im-demo/perceptual-standard.jpg?imbypass=true"></NewsCard>
-            <NewsCard title="Lorem ipsum" description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec ipsum massa, ullamcorper in." imgSrc="https://www.akamai.com/site/im-demo/perceptual-standard.jpg?imbypass=true"></NewsCard>
-            <NewsCard title="Lorem ipsum" description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec ipsum massa, ullamcorper in." imgSrc="https://www.akamai.com/site/im-demo/perceptual-standard.jpg?imbypass=true"></NewsCard>
+        <div className="p-10">
+            <div className="flex gap-6">
+                <NewsCard title="Lorem ipsum" description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec ipsum massa, ullamcorper in." imgSrc="https://www.akamai.com/site/im-demo/perceptual-standard.jpg?imbypass=true"></NewsCard>
+                <NewsCard title="Lorem ipsum" description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec ipsum massa, ullamcorper in." imgSrc="https://www.akamai.com/site/im-demo/perceptual-standard.jpg?imbypass=true"></NewsCard>
+                <NewsCard title="Lorem ipsum" description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec ipsum massa, ullamcorper in." imgSrc="https://www.akamai.com/site/im-demo/perceptual-standard.jpg?imbypass=true"></NewsCard>
+            </div>
+            <div className="mt-8">
+                <h2 className="text-xl font-bold">Na co máte náladu?</h2>
+                <div className="mt-4 flex gap-6">
+                    <ActivityCard title="Cyklistika" imgSrc="https://www.akamai.com/site/im-demo/perceptual-standard.jpg?imbypass=true"></ActivityCard>
+                    <ActivityCard title="Turistika" imgSrc="https://www.akamai.com/site/im-demo/perceptual-standard.jpg?imbypass=true"></ActivityCard>
+                    <ActivityCard title="Kluby" imgSrc="https://www.akamai.com/site/im-demo/perceptual-standard.jpg?imbypass=true"></ActivityCard>
+                    <ActivityCard title="Cyklistika" imgSrc="https://www.akamai.com/site/im-demo/perceptual-standard.jpg?imbypass=true"></ActivityCard>
+                    <ActivityCard title="Cyklistika" imgSrc="https://www.akamai.com/site/im-demo/perceptual-standard.jpg?imbypass=true"></ActivityCard>
+                    <ActivityCard title="Cyklistika" imgSrc="https://www.akamai.com/site/im-demo/perceptual-standard.jpg?imbypass=true"></ActivityCard>
+                </div>
+            </div>
         </div>
     );
 }
