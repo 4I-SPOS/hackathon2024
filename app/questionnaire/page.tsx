a3c9ba44c11efd6ee85232d2b54712b207a79f77
Sinner <64489962+SinnerK0N@users.noreply.github.com>
2024-10-14 19:00:17 +0200

    Added more questions to questionnaire

--- app/questionnaire/page.tsx
+++ app/questionnaire/page.tsx
@@ -23,26 +23,110 @@ type Answer =
 
 type Activity =
 {
-    name: string;
     id: Activities;
     score: number;
 }
 enum Activities
 {
-    Cyklistika,
-    Turistika,
-    Kluby
+    Cyklistika, //cyklovylety
+    Turistika, //rozhledny_a_vyhlidky
+    Kultura, //kina, kulturni_domy, muzea_a_galerie
+    Wellness, //lazne, solne_jeskyne
+    Pamatky, //architektonicke_pamatky, hrady, muzea_a_galerie, narodni_kulturni_pamatky, ostatni_historicke_pamatky, pamatkove_rezervy, pamatkove_zony, technicke_pamatky, valecne_hroby_?, zamky
+    Pivovary, //pivovary
+    Rybareni //rybareni
 }
 const allActivities: Activity[] = [
-    { name: "Cyklistika", id: Activities.Cyklistika, score: 0 },
-    { name: "Turistika", id: Activities.Turistika, score: 0 },
-    { name: "Kluby", id: Activities.Kluby, score: 0 }
+    { id: Activities.Cyklistika, score: 0 },
+    { id: Activities.Turistika, score: 0 },
+    { id: Activities.Kultura, score: 0 },
+    { id: Activities.Wellness, score: 0 },
+    { id: Activities.Pamatky, score: 0 },
+    { id: Activities.Pivovary, score: 0 },
+    { id: Activities.Rybareni, score: 0 }
 ]
 
+function determineActivityByID(id: number): string
+{
+    switch (id)
+    {
+        case Activities.Cyklistika:
+            return "Cyklistika";
+        case Activities.Turistika:
+            return "Turistika";
+        case Activities.Kultura:
+            return "Kultura";
+        case Activities.Wellness:
+            return "Wellness";
+        case Activities.Pamatky:
+            return "Pamatky";
+        case Activities.Pivovary:
+            return "Pivovary";
+        case Activities.Rybareni:
+            return "Rybareni";
+        default:
+            return "???"
+    }
+}
+
 const questions: Question[] = [
-    { question: "Radši trávíte čas", firstAnswer: { answer: "V přírodě", scores: [ { id: Activities.Cyklistika, score_increase: 1 } ] }, secondAnswer: { answer: "Vevnitř", scores: [ { id: Activities.Kluby, score_increase: 1 } ] } },
-    { question: "Máte radši", firstAnswer: { answer: "Hory", scores: [ { id: Activities.Turistika, score_increase: 2 }, { id: Activities.Cyklistika, score_increase: 0.5 } ] }, secondAnswer: { answer: "Moře", scores: [ { id: Activities.Cyklistika, score_increase: 1 } ] } },
-    { question: "Pijete rádi alkohol?", firstAnswer: { answer: "Ano", scores: [ { id: Activities.Kluby, score_increase: 2 } ] }, secondAnswer: { answer: "Ne", scores: [ ] } }
+    { question: "Preferujete aktivní pohyb nebo relaxaci?",
+        firstAnswer: { answer: "Aktivní pohyb", scores:
+                [
+                    { id: Activities.Cyklistika, score_increase: 1 },
+                    { id: Activities.Turistika, score_increase: 1 }
+                ]},
+        secondAnswer: { answer: "Relaxaci", scores:
+                [
+                    { id: Activities.Kultura, score_increase: 0.5 },
+                    { id: Activities.Wellness, score_increase: 1 },
+                    { id: Activities.Pivovary, score_increase: 1 },
+                    { id: Activities.Rybareni, score_increase: 1 }
+                ]}
+    },
+    { question: "Raději trávíte čas venku v přírodě nebo uvnitř?",
+        firstAnswer: { answer: "Venku v přírodě", scores:
+                [
+                    { id: Activities.Turistika, score_increase: 1 },
+                    { id: Activities.Cyklistika, score_increase: 1 },
+                    { id: Activities.Rybareni, score_increase: 1 }
+                ] },
+        secondAnswer: { answer: "Uvnitř", scores:
+                [
+                    { id: Activities.Kultura, score_increase: 1 },
+                    { id: Activities.Wellness, score_increase: 1 }
+                ] } },
+    { question: "Chcete si odpočinout a pečovat o své tělo, nebo zažít něco nového a poznávat okolí?",
+        firstAnswer: { answer: "Odpočinout a pečovat o své tělo", scores:
+                [
+                    { id: Activities.Wellness, score_increase: 1 }
+                ] },
+        secondAnswer: { answer: "Zažít něco nového a poznávat okolí", scores:
+                [
+                    { id: Activities.Kultura, score_increase: 1 },
+                    { id: Activities.Turistika, score_increase: 1 },
+                ] } },
+    { question: "Chtěli byste se dozvědět více o historii a kultuře, nebo dáváte přednost pobytu v přírodě?",
+        firstAnswer: { answer: "Historii a kultuře", scores:
+                [
+                    { id: Activities.Kultura, score_increase: 1 },
+                ] },
+        secondAnswer: { answer: "Pobytu v přírodě", scores:
+                [
+                    { id: Activities.Turistika, score_increase: 1 },
+                    { id: Activities.Cyklistika, score_increase: 1 },
+                    { id: Activities.Rybareni, score_increase: 1 }
+                ] } },
+    { question: "Představujete si ideální den jako strávený v sauně a na masáži, nebo spíše na horské stezce?",
+        firstAnswer: { answer: "V sauně a na masáži", scores:
+                [
+                    { id: Activities.Wellness, score_increase: 1 }
+                ] },
+        secondAnswer: { answer: "Na horské stezce", scores:
+                [
+                    { id: Activities.Turistika, score_increase: 1 },
+                    { id: Activities.Cyklistika, score_increase: 0.5 }
+                ] } }
 ]
 
 export default function Home()
@@ -56,7 +140,7 @@ export default function Home()
         recommendedActivities = recommendedActivities.filter(a => a.score > 1.5); //tohle se pak bude delat dynamicky (nejlepsi match & -x score)
         setTimeout(() => //pouze pro debuggovani - zavolani alert normalne zablokuje thread a zastavovalo tak animace - to me stvalo
         {
-            alert(recommendedActivities.map(a => `${a.name} - ${a.score}`).join(", "));
+            alert(recommendedActivities.map(a => `${determineActivityByID(a.id)} - ${a.score}`).join(", "));
         }, 10);
     }
     const NextQuestion = () =>
