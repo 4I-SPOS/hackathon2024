9942b567ba8b5c71246f3c4faf80279f352696a0
Sinner <64489962+SinnerK0N@users.noreply.github.com>
2025-03-06 23:52:23 +0100

    Fix <Image> alt property missing warning

--- app/components/NewsCard.tsx
+++ app/components/NewsCard.tsx
@@ -15,7 +15,7 @@ export default function NewsCard({ imgSrc, title, description, link }: NewsCardP
     return (
         <Link className="w-1/3 transition hover:scale-[1.02]" href={link} target="_blank" rel="noopener noreferrer" passHref>
             <Card style={{ cursor: 'pointer' }}>
-                <Image src={imgSrc} className="aspect-video object-cover" />
+                <Image src={imgSrc} className="aspect-video object-cover" alt={title} />
                 <CardFooter className="flex flex-col items-start text-black">
                     <h2 className="font-bold text-lg">{title}</h2>
                     <p className="mt-3 text-neutral-500 line-clamp-2">{description}</p>
